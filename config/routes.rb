c0058d7a22034fe16a9b08273f1dded77581f8b1
bill-chase-kellogg <bill.chase@kellogg.northwestern.edu>
2022-02-19 03:53:44 +0000

    places should be done - moving on to posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-root to: 'places#index'
+root 'places#index'
 resources: places
 resources: posts
 
