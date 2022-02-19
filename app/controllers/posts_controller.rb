9c098e143a43a57979a1f3c4e0e3bbd2ac38ed53
bill-chase-kellogg <bill.chase@kellogg.northwestern.edu>
2022-02-19 04:39:08 +0000

    Screwed up my server somehow - restarting gitpod

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,22 @@
 class PostsController < ApplicationController
 
-    def index
-        @posts = Post.all
-    end
+    # def index
+    #     @posts = Post.all
+    # end
+
+    # def show
+    #     # .find(123) is the same as .where({ id: 123 })[0]
+    #     @place = Place.find(params["id"])
+    # end
+
+    def new
+        @post = Post.new
+      end
     
+      def create
+        @post = Post.new(params["post"])
+        @post.save
+        redirect_to @place
+      end
+
 end
